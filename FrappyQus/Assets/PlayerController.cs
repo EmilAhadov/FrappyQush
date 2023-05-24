8653032ccb5fe13aa5ec18c75cea4706b49256f1
Turan Heydarli <turan.heydarli@code.edu.az>
2023-05-25 00:10:06 +0400

    add save system

--- FrappyQus/Assets/PlayerController.cs
+++ FrappyQus/Assets/PlayerController.cs
@@ -1,9 +1,30 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float jumpForce;
 
+    private static PlayerController _instance;
+    public static PlayerController Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<PlayerController>();
+            }
+
+            return _instance;
+        }
+    }
+
+    public int Score => _score;
+
+    public event Action OnScoreUpdate;
+    public event Action OnGameLoose;
+
+    private int _score;
     private Rigidbody2D _rigidbody;
 
     void Start()
@@ -13,9 +34,27 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetTouch(0).phase == TouchPhase.Began)
         {
             _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Pipe"))
+        {
+            _score++;
+            OnScoreUpdate?.Invoke();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            OnGameLoose?.Invoke();
+            Time.timeScale = 0;
         }
     }
 }
